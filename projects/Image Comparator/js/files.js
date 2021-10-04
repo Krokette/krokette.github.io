ad0d843cd550a30c026ce978a0b68a8d918108f9
Krokette <65606052+Krokette@users.noreply.github.com>
2021-10-04 18:44:44 +0200

    add todo

--- projects/Image Comparator/js/files.js
+++ projects/Image Comparator/js/files.js
@@ -7,3 +7,6 @@ var loadFileAfter = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
     initComparisons();
 };
+
+//TODO Maybe I should try to implement his solution for the comparison slider. It works way better than the current version of mine
+// http://adrianlarsson.com/stuff/gw2rtgi
